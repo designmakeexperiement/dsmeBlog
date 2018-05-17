36d6c18076579fa638ab6bc5ded1c8478c8504b2
Jordan Hiatt <jordan.hiatt11@gmail.com>
2018-05-17 13:39:48 -0400

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// main app
+import App from './src/components/App';
+import reducers from './src/reducers';
+
+ReactDOM.render(
+  <App />
+  , document.getElementById('app'));
